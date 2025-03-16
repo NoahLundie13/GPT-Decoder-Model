272ecd7c83ae672efff3b827c3fac9485ae7d630
Noah Lundie <noah.lundie@shaw.ca>
2025-03-15 21:07:43 -0700

    Finished training file

--- train.py
+++ train.py
@@ -2,12 +2,16 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-batch_size = 32
-block_size = 8
-max_iters = 3000
-eval_interval = 300
-learning_rate = 0.01
+batch_size = 64
+block_size = 256
+max_iters = 5000
+eval_interval = 500
+learning_rate = 0.0001
 eval_iters = 200
+n_embd = 384
+n_layer = 6
+n_head = 6
+dropout = 0.2
 torch.manual_seed(1337)
 
 with open("input.txt", "r", encoding='utf-8') as f:
@@ -49,13 +53,87 @@ def estimate_loss():
     model.train()
     return out
 
-class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size):
+class Head(nn.Module):
+    def __init__(self, head_size):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        tril_tensor = torch.tril(torch.ones(block_size, block_size, dtype=torch.bool))
+        self.register_buffer("tril", tril_tensor)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+
+        weights = q @ k.transpose(-2, -1) * C**-0.5
+        weights = weights.masked_fill(self.tril[:T, :T]== 0, float('-inf'))  # Apply mask
+        weights = F.softmax(weights, dim=-1)
+        weights = self.dropout(weights)
+
+        v = self.value(x)
+        out = weights @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        out = self.dropout(out)
+        return out
+
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd,  4 * n_embd),
+            nn.ReLU(), 
+            nn.Linear(4 * n_embd, n_embd), 
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
-        logits = self.token_embedding_table(idx)  # (Batch, Time, Channels)
+        B, T = idx.shape 
+
+        tok_emb = self.token_embedding_table(idx)  # (Batch, Time, N_Emb)
+        pos_emb = self.position_embedding_table(torch.arange(T))  # (T, C)
+        x = tok_emb + pos_emb  # (B, T, C)
+        x = self.blocks(x)
+        logits = self.lm_head(x)  # (Batch, Time, Vocab_Size)
 
         if targets == None:
             loss = None
@@ -70,7 +148,8 @@ class BigramLanguageModel(nn.Module):
 
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
-            logits, loss = self.forward(idx)
+            idx_cond = idx[:, -block_size:]
+            logits, loss = self.forward(idx_cond)
 
             logits = logits[:, -1, :]
 
@@ -81,7 +160,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 for iter in range(max_iters):
